a89692765a14365153953a22059c202807859c56
evananyonga <evewish@gmail.com>
2021-05-11 01:52:10 +0300

    map project views to urls

---
+++ crudblog/crudblog/eblog/urls.py
@@ -0,0 +1,7 @@
+from . import views
+from djang.urls import path
+
+urlpatterns = [
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+]
