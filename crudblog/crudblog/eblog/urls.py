6632b3b9ceca64b5eb3c4e54e51568d3804c8311
evananyonga <evewish@gmail.com>
2021-05-19 12:35:24 +0300

    Add project files

--- crudblog/crudblog/eblog/urls.py
+++ crudblog/crudblog/eblog/urls.py
@@ -1,7 +1,12 @@
-from . import views
-from djang.urls import path
+from .views import PostList, PostDetail, UpdatePost, CreatePost, DeletePost, CreateUser
+from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('', PostList.as_view(), name='home'),
+    path('register/', CreateUser.as_view(), name='register-user'),
+    path('<slug:slug>/', PostDetail.as_view(), name='post_detail'),
+    path('posts/', CreatePost.as_view(), name='add-post'),
+    path('posts/<int:pk>/', UpdatePost.as_view(), name='update-post'),
+    path('posts/<int:pk>/delete', DeletePost.as_view(), name='delete-post'),
+
 ]
--- crudblog/crudblog/eblog/views.py
+++ crudblog/crudblog/eblog/views.py
@@ -1,14 +1,66 @@
-from django.shortcuts import render
-from django.views import generic
-from .models import Post
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, edit
+from django.urls import reverse_lazy
+from .models import Post, User
+from .forms import RegisterForm
+from django.contrib.auth import login
+from django.contrib import messages
 
 
-# Create your views here.
-class PostList(generic.ListView):
-    queryset = Post.objectives.filter(status=1).order_by('-created_on')
+class PostList(ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
 
 
-class PostDetail(generic.DetailView):
+class PostDetail(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+class UpdatePost(edit.UpdateView):
+    model = Post
+    template_name = 'post_detail.html'
+    fields = ['content']
+    success_url = reverse_lazy('post_detail.html')
+
+
+class DeletePost(edit.DeleteView):
+    model = Post
+    template_name = 'post_detail.html'
+    success_url = reverse_lazy('post_detail.html')
+
+
+class CreatePost(edit.CreateView):
+    model = Post
+    template_name = 'create_post.html'
+    fields = [
+        'post',
+        'user'
+    ]
+    success_url = reverse_lazy('post_detail.html')
+
+
+class CreateUser(edit.CreateView):
+#     def register_request(request):
+#         if request.method == "POST":
+#             form = User(request.POST)
+#             if form.is_valid():
+#                 user = form.save()
+#                 login(request, user)
+#                 messages.success(request, 'Registration successful')
+#                 return redirect("home")
+#             messages.error(request, "Unsuccessful Registration, Invalid information")
+#         form = User
+#         return render(request=request, template_name="register.html", context={"register_form":form})
+#
+#
+# def get_query_set(self):
+#     return User.objects.order_by['-date_added']
+
+    model = User
+    fields = [
+        'username',
+        'email',
+        'password'
+    ]
+    template_name = 'register.html'
